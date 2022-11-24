e46d375fd04309e427f1759adbf4b81dcceb5f53
RahatBinOamr <108364543+RahatBinOamr@users.noreply.github.com>
2022-11-24 15:42:22 +0600

    crate mongodb atlas and load category data

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config()
 const app = express()
@@ -7,6 +8,32 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
+
+
+
+const uri = process.env.USER_URL;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+function run (){
+try{
+    const categoryCollection = client.db('usedPhone').collection('category')
+
+    app.get("/CoursesCategory",async(req,res)=>{
+        const filter = {}
+        const result = await  categoryCollection.find(filter).toArray()
+        console.log(result)
+        res.send(result)
+      })
+
+
+
+
+}
+finally{
+
+}
+}
+run()
 
 app.get('/',(req,res)=>{
     res.send(`server is running ${port}`)
