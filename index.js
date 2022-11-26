4d72b0421b170de3835a21ce9ddb5d8a513ba1ab
RahatBinOamr <108364543+RahatBinOamr@users.noreply.github.com>
2022-11-26 17:45:25 +0600

    create admin usint put method

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
+var jwt = require('jsonwebtoken');
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.USER_URL;
-console.log(uri);
+// console.log(uri);
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -44,14 +45,14 @@ function run() {
     app.get("/phones", async (req, res) => {
       const filter = {};
       const result = await phonesCollection.find(filter).toArray();
-      console.log(result);
+      // console.log(result);
       res.send(result);
     });
     app.get("/phones/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { _id: ObjectId(id) };
       const phonesId = await phonesCollection.findOne(filter);
-      console.log(phonesId);
+      // console.log(phonesId);
       res.send(phonesId);
     });
     app.get("/category", async (req, res) => {
@@ -68,46 +69,33 @@ function run() {
         const filter = {};
         const result = await phonesCollection.find(filter).toArray();
         res.send(result);
-        console.log(result);
+        // console.log(result);
       } 
       else{
         const categoryId = await phonesCollection.find(query).toArray()
-        console.log(categoryId)
+        // console.log(categoryId)
         res.send(categoryId)
       }
     });
 
-    app.get('/bookings',  async (req, res) => {
+    app.get('/bookings',verifyJWT,  async (req, res) => {
       const email = req.query.email;
-      // const decodedEmail = req.decoded.email;
+      const decodedEmail = req.decoded.email;
 
-      // if (email !== decodedEmail) {
-      //     return res.status(403).send({ message: 'forbidden access' });
-      // }
+      if (email !== decodedEmail) {
+          return res.status(403).send({ message: 'forbidden access' });
+      }
 
       const query = { email: email };
       const bookings = await bookingsCollection.find(query).toArray();
+      // console.log(bookings);
       res.send(bookings);
   })
 
     /* Booking collection */
     app.post('/bookings', async (req, res) => {
       const booking = req.body;
-      console.log(booking);
-      const query = {
-          date: booking.date,
-          email: booking.email,
-          title: booking.title,
-          price:booking.price
-      }
-
-      const alreadyBooked = await bookingsCollection.find(query).toArray();
-
-      if (alreadyBooked.length){
-          const message = `You already have a booking on ${booking.date}`
-          return res.send({acknowledged: false, message})
-      }
-
+      // console.log(booking);
       const result = await bookingsCollection.insertOne(booking);
       res.send(result);
   })
@@ -123,13 +111,42 @@ function run() {
     res.status(403).send({ accessToken: '' })
 });
 
-    /* User Collection */
+    /* User info */
     app.post('/users', async (req, res) => {
       const user = req.body;
       console.log(user);
       const result = await usersCollection.insertOne(user);
       res.send(result);
   });
+  /* user Collection */
+  app.get('/users', async (req, res) => {
+    const query = {};
+    const users = await usersCollection.find(query).toArray();
+    res.send(users);
+});
+/* handel make admin */
+app.put('/users/admin/:id', async (req, res) => {
+  const decodedEmail = req.decoded.email;
+  const query = { email: decodedEmail };
+  const user = await usersCollection.findOne(query);
+
+  if (user?.role !== 'admin') {
+      return res.status(403).send({ message: 'forbidden access' })
+  }
+
+  const id = req.params.id;
+  const filter = { _id: ObjectId(id) }
+  const options = { upsert: true };
+  const updatedDoc = {
+      $set: {
+          role: 'admin'
+      }
+  }
+  const result = await usersCollection.updateOne(filter, updatedDoc, options);
+  console.log(result)
+  res.send(result);
+})
+
   } finally {
   }
 }
